5c4eaef997655f5a6a34d94ce8b7f258dc1f8ab7
Orlando Verdin <git@orli.land>
2024-05-10 19:02:22 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>My todo app</h1>>
-=======
       <h1>My todo app</h1>
->>>>>>> a4aa21d (clean up code base of vite)
     </>
   );
 }
