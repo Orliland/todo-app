dd186cf3c0fca43a4af96b4ddde75b4af044094f
Orlando Verdin <git@orli.land>
2024-05-10 19:00:31 -0700

    clean up code base of vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <h1>My todo app</h1>>
+=======
+      <h1>My todo app</h1>
+>>>>>>> a4aa21d (clean up code base of vite)
     </>
   );
 }
