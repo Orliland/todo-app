ca80c351bca90df465bc517c77ad665f7718cb06
Orlando Verdin <git@orli.land>
2024-05-11 22:26:18 -0700

    add check task handler

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,18 +1,16 @@
 import IconCross from "../../assets/icon-cross.svg";
 import "./task.css";
 
-function InputCheck({ done }) {
-  if (done) {
-    return <input type="checkbox" className="task__check" checked />;
-  } else {
-    return <input type="checkbox" className="task__check" />;
-  }
-}
-
-export default function Task({ name, done }) {
+export default function Task({ id, name, done, onChecked }) {
   return (
     <div className="task">
-      <InputCheck done={done} />
+      <input
+        type="checkbox"
+        className="task__check"
+        onChange={() => {
+          onChecked(id);
+        }}
+      />
       <label className="task__label">{name}</label>
       <button className="task__delete">
         <img src={IconCross} alt="delete task" className="task__delete-icon" />
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,10 +1,16 @@
 import Task from "../Task";
 
-export default function Tasks({ tasks }) {
+export default function Tasks({ tasks, onChecked }) {
   return (
     <div>
       {tasks.map((task) => (
-        <Task key={task.id} name={task.name} done={task.done} />
+        <Task
+          key={task.id}
+          id={task.id}
+          name={task.name}
+          done={task.done}
+          onChecked={onChecked}
+        />
       ))}
     </div>
   );
