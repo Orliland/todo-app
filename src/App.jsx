4e210e892b5573950953364dacb6d798a1bb6863
Orlando Verdin <git@orli.land>
2024-05-11 21:55:41 -0700

    add db task state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./App.css";
-import Task from "./components/Task";
+
+const initialTasks = [
+  {
+    id: 1,
+    name: "Jog around the park 3x",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "10 minutes meditation",
+    completed: false,
+  },
+  {
+    id: 3,
+    name: "Read for 1 hour",
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Pick up groceries",
+    completed: false,
+  },
+  {
+    id: 5,
+    name: "Complete Todo App on Frontend Mentor",
+    completed: false,
+  },
+];
+
+let nextId = 6;
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
   return (
     <>
-      <Task />
+      <Tasks tasks={tasks} />
     </>
   );
 }
